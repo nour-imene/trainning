33235b0f2afe86069179f393c6dd72b3eb408f36
nour imene <sdj.nourimene@gmail.com>
2022-10-14 17:31:45 +0300

    part two

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
